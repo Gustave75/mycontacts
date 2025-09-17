d3869654efa67de5183b95a7e87c142c927001b6
gustave <gustave.jacq@gmail.com>
2025-09-17 11:44:57 +0200

    MongoDB init

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Route test
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello from server 🚀' });
+  res.send({ message: 'Hello from server' });
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
