9754543623fb546f52e3054a5894e6ecb32f61f6
gustave <gustave.jacq@gmail.com>
2025-09-17 11:11:44 +0200

    chore: init monorepo mycontacts

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Route test
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello from server 🚀' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
